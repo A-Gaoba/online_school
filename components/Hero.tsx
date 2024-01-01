367e9f15bddb61d271125f8686c5e0c907b378ff
a-goaba <abdulrahman1997abdo@gmail.com>
2024-01-01 22:00:54 +0500

    navbar, hero and footer had done

---
+++ components/Hero.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Image from "next/image";
+import Button from "./Button";
+
+
+const Hero = () => {
+  return (
+    <div className="container mx-auto my-2 p-4">
+      <div className="bg-[#e9edc9] px-6 lg:px-16 py-4 rounded-3xl ">
+        <Navbar />
+        <div className="flex flex-col lg:flex-row justify-center mt-8 items-center gap-5 lg:gap-10 2xl:p-24 xl:p-10">
+          <div className="flex flex-col justify-center lg:w-1/2 gap-6 md:gap-10">
+            <p className="text-2xl sm:text-4xl md:text-5xl font-bold">
+              Igniting Minds, Inspiring Futures
+            </p>
+            <p className="text-sm sm:text-base lg:text-lg text-gray-500">
+              At <span className="font-bold">ALNAHDAH SCHOOL</span>, we ignite curiosity and shape futures. With
+              devoted educators and an environment fostering innovation, we
+              empower students to grow, collaborate, and excel. Join our
+              transformative educational journey, where every student&apos;s
+              potential is unleashed.
+            </p>
+            
+            <Button
+              type="button"
+              title="Get Started"
+              variant="btn_dark_green"
+            />
+          </div>
+          <div className="lg:w-1/2">
+            {/* <img src="/assets/hero.png" alt="" className="w-full h-auto" /> */}
+            <Image src="/heroImage.png" alt="hero image" layout="responsive" width={190} height={100} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ components/Navbar.tsx
@@ -0,0 +1,102 @@
+"use client";
+import React, { useState, useRef, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Button from "./Button";
+import { NAV_LINKS } from "@/constants";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [menuIcon, setMenuIcon] = useState("/menu.svg");
+  const menuRef = useRef<HTMLDivElement | null>(null);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+    const newIcon = isOpen ? "/menu.svg" : "/close.svg";
+    setMenuIcon(newIcon);
+  };
+
+  const closeMenu = (e: MouseEvent) => {
+    if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
+      setIsOpen(false);
+      setMenuIcon("/menu.svg");
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", closeMenu);
+    return () => {
+      document.removeEventListener("mousedown", closeMenu);
+    };
+  }, []);
+
+  return (
+    <nav className="flexBetween relative z-30 lg:py-2 py-5">
+      <Link href="/">
+        <Image src="/logo1.png" alt="logo" width={84} height={24} />
+      </Link>
+
+      {/* Responsive Menu */}
+      <div className="lg:hidden ">
+        <Image
+          src={menuIcon}
+          alt="menu"
+          width={24}
+          height={24}
+          className="inline-block cursor-pointer"
+          onClick={toggleMenu}
+        />
+        {isOpen && (
+          <div className="absolute flex top-full right-0 bg-[#e9edc9] p-4 rounded w-40">
+            <ul className="flex flex-col items-start space-y-4">
+              {NAV_LINKS.map((link) => (
+                <li key={link.key}>
+                  <Link
+                    href={link.href}
+                    key={link.key}
+                    className="regular-16 text-gray-50 cursor-pointer transition-all hover:font-bold"
+                  >
+                    <p>{link.label}</p>
+                  </Link>
+                </li>
+              ))}
+              <Button
+                type="button"
+                title="login"
+                icon="/user.svg"
+                variant="btn_login"
+              />
+            </ul>
+          </div>
+        )}
+      </div>
+
+      {/* Regular Menu */}
+      <ul className="hidden lg:flex h-full gap-12">
+        {NAV_LINKS.map((link) => (
+          <li key={link.key}>
+            <Link
+              href={link.href}
+              key={link.key}
+              className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+            >
+              <p>{link.label}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      {/* Login Button */}
+      <div className="lg:flexCenter hidden">
+        <Button
+          type="button"
+          title="login"
+          icon="/user.svg"
+          variant="btn_green"
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
